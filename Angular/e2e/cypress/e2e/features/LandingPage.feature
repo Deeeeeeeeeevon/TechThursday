2c6fada86ed68bd75279f76e04df72667728a5ab
Devon Fraser <fraserd1@LEGAL.REGN.NET>
2023-06-15 15:30:33 +0200

    updated tests

--- Angular/e2e/cypress/e2e/features/LandingPage.feature
+++ Angular/e2e/cypress/e2e/features/LandingPage.feature
@@ -2,8 +2,33 @@ Feature: LandingPage Functionality
 
     This Feature tests login scenarios
 
-    Background: 
+    Background:
         Given the user is on the landing page
 
     Scenario: As a toh user, I want to navigate to landingpage in order to see the dashboard
         Then the user shall see the employee dashboard
+
+    Scenario: As a TOH user, I want to navigate to dashboard in order to see top 4 Heroes displayed
+        Then the user shall see the top 4 Heroes displayed
+
+    Scenario: As a TOH user, I want to select a hero from the dashboard in order to see the hero details displayed
+        When the user selects a hero from the dashboard
+        Then the hero's details are displayed
+
+    Scenario: As a TOH user, I want to navigate to heroes page in order to see 8 heroes
+        When the user clicks the heros button
+        Then the user is navigated to the heroes page.
+
+    Scenario: As a TOH user, I want to remove a hero in order to see the hero list decrease
+        When the user clicks the delete button next to hero
+        Then the hero is deleted
+
+    Scenario: As a TOH user, I want to add a hero in order to see the hero list increase
+        When the user clicks the Add button next to hero
+        And the hero has a name
+        Then the hero is added
+
+    Scenario: As a TOH user, I want to add a hero without a name in order to see the hero list remain the same
+        When the user clicks the Add button next to hero
+        And the hero has no name
+        Then the hero is not added
--- Angular/e2e/cypress/e2e/step-definitions/LandingPage.spec.ts
+++ Angular/e2e/cypress/e2e/step-definitions/LandingPage.spec.ts
@@ -1,9 +1,83 @@
 import { Given, When, Then, And} from "cypress-cucumber-preprocessor/steps";
 
+var cnt: number;
+
 Given(/^the user is on the landing page$/, () => {
-	cy.visit("http://localhost:4200/dashboard");
+    cy.visit("http://localhost:4200/dashboard");
 });
 
 Then(/^the user shall see the employee dashboard$/, () => {
-	return true;
+    return true;
+});
+
+Then(/^the user shall see the top 4 Heroes displayed$/, () => {
+    cy.get('.heroes-menu').children().should('have.length', 4)
+});
+
+When(/^the user selects a hero from the dashboard$/, () => {
+    cy.get('.heroes-menu').children().first().click();
+});
+
+Then(/^the hero's details are displayed$/, () => {
+    cy.url().should('include', 'http://localhost:4200/detail')
+});
+
+When(/^the user clicks the heros button$/, () => {
+    cy.get('a[href="/heroes"]').click();
+});
+
+Then(/^the user is navigated to the heroes page.$/, () => {
+    cy.url().should('include', 'http://localhost:4200/heroes')
+    cy.get('.heroes').children().then($elements =>
+    {
+        cy.get('.heroes').children().should('have.length', $elements.length)
+    })
+});
+
+When(/^the user clicks the delete button next to hero$/, () => {
+    cy.get('a[href="/heroes"]').click();
+    cy.get('.heroes').children().then($elements =>
+    {
+        cnt = $elements.length
+    })
+
+    cy.get('.delete').first().click();
+});
+
+Then(/^the hero is deleted$/, () => {
+    cy.get('.heroes').children().should('have.length', cnt-1)
+});
+
+When(/^the user clicks the Add button next to hero$/, () => {
+    cy.get('a[href="/heroes"]').click();
+    cy.get('.heroes').children().then($elements =>
+    {
+        cnt= $elements.length
+    })
+});
+
+When(/^the hero has a name$/, () => {
+    cy.get('[id=new-hero]').type("Helo World")
+    cy.get('.add-button').first().click();
+});
+
+Then(/^the hero is added$/, () => {
+    cy.get('.heroes').children().should('have.length', cnt+1)
+});
+
+When(/^the user clicks the Add button next to hero$/, () => {
+    cy.get('a[href="/heroes"]').click();
+    cy.get('.heroes').children().then($elements =>
+    {
+        cnt = $elements.length
+    })
+});
+
+When(/^the hero has no name$/, () => {
+    cy.get('[id=new-hero]').clear()
+    cy.get('.add-button').first().click();
+});
+
+Then(/^the hero is not added$/, () => {
+    cy.get('.heroes').children().should('have.length', cnt)
 });
